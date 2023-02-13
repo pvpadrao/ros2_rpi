c5430f2a94a1a9b9a36ee76534555f0fcf1360eb
pvpadrao <pv.padrao@gmail.com>
2023-02-13 13:10:16 -0500

    ..

---
+++ vision_rpi_bot/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'vision_rpi_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='plope113',
+    maintainer_email='pv.padrao@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
